65dad9959d9dd638097cba66de345a8dfc281e6d
HongShen Zhao <62940257+HongShen-Z@users.noreply.github.com>
2023-06-07 14:40:45 +0800

    Update train_cifar10.py

--- train_cifar10.py
+++ train_cifar10.py
@@ -62,7 +62,7 @@ bs = int(args.bs)
 imsize = int(args.size)
 
 use_amp = bool(~args.noamp)
-aug = args.noaug
+aug = not args.noaug
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
